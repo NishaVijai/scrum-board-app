c9a90c46af89a2300976089641fe8c4f225f1cc2
Nisha <nishavijai@gmail.com>
2025-08-10 18:03:29 +0200

    update Board.tsx

--- client/src/components/Board.tsx
+++ client/src/components/Board.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import { List } from './List';
@@ -7,36 +6,6 @@ import { type List as ListType } from '../types';
 
 export const Board = () => {
   const lists = useBoardStore((state) => state.lists);
-  const loadTasksFromBackend = useBoardStore((state) => state.loadTasksFromBackend);
-
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const init = async () => {
-      setLoading(true);
-      await loadTasksFromBackend();
-      setLoading(false);
-    };
-    init();
-  }, [loadTasksFromBackend]);
-
-  if (loading) {
-    return (
-      <div className="board-loading">
-        <p>Loading board...</p>
-      </div>
-    );
-  }
-
-  const isEmpty = lists.every((list) => list.cards.length === 0);
-
-  if (isEmpty) {
-    return (
-      <div className="board-empty">
-        <p>No tasks found. Start by adding a card!</p>
-      </div>
-    );
-  }
 
   return (
     <DndProvider backend={HTML5Backend}>
